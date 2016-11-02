7b05a4b6cebc3a01fcee5e1a6a5ebe4a4791dd58
raymondokb <raymondokbmooc@gmail.com>
2016-11-02 11:32:42 +0800

    begin3

--- moudle4w1.r
+++ moudle4w1.r
@@ -19,9 +19,28 @@ install_from_swirl("Exploratory Data Analysis")
 #rug(ppm): grayscale representation of histogram, shows concentration density
 #hist(ppm, col = "green", breaks=100)
 #abline(v=12, lwd=2) draws a vertical line,of thickness 2
+#abline(v=median(ppm), col="magenta", lwd=4)
+#names(pollution)
 
+#barplot(reg, col = "wheat", main = "Number of Counties in Each Region")
+#boxplot( pm25 ~ region, data = pollution, col = "red")
+#
+#par(mfrow=c(2,1),mar=c(4,4,2,1)) is two rows, one column
+#east <- subset(pollution, region=="east")
+#hist(east$pm25, col = "green")
+#hist(subset(pollution, region=="west")$pm25, col="green")
 
+#################
+#with(pollution, plot(latitude, pm25)) IMPORTANT SHORTCUT
+#################
+#abline(h=12, lwd = 2, lty = 2); lty is type of line, lwd is thickness
+#plot(x = pollution$latitude, y = ppm, col = pollution$region)
+#
 
-
+#par(mfrow = c(1, 2), mar= c(5, 4, 2, 1))
+#west <- subset(pollution, region == "west")
+#plot(west$latitude, west$pm25, main = "West")
+#plot(east$latitude, east$pm25, main = "East")
+#
 
 
