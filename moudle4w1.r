e425d660b6ee00508f71b7384350312a275cd709
raymondokb <raymondokbmooc@gmail.com>
2016-11-02 10:40:46 +0800

    begin2

--- moudle4w1.r
+++ moudle4w1.r
@@ -1,1 +1,27 @@
 library(ggplot2)
+#SImpson's paradox: trend appears in different groups of data but disappears or reverses when these groups are combined
+
+# Rules
+# 1. Show comparisons
+# 2. Show causalit, mechanism, explanation
+# 3. Show multivariate data
+# 4. Integrate multiple modes of evidence
+# 5. Describe and document the evidence (source code)
+# 6. Content is king
+
+library(swirl)
+install_from_swirl("Exploratory Data Analysis")
+
+#quantile(vector) gets 0, 25, 50, 75, 100%
+#boxplot(ppm, col="blue")
+#abline(h=12) draws horizontal line at value 12. Simple quick "indicator" to prove a point on a plot
+#hist(ppm, col = "green")
+#rug(ppm): grayscale representation of histogram, shows concentration density
+#hist(ppm, col = "green", breaks=100)
+#abline(v=12, lwd=2) draws a vertical line,of thickness 2
+
+
+
+
+
+
