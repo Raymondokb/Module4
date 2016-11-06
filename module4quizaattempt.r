aba14e75372f7c6fe42d1474f971a4fd71d41613
Raymondokb <raymondokbmooc@gmail.com>
2016-11-06 22:00:24 +0800

    Add files via upload

--- module4quizaattempt.r
+++ module4quizaattempt.r
@@ -53,5 +53,20 @@ with(subset(airquality, Month != 5), points(Wind, Ozone, col = "red"))
 legend("topright", pch = 1, col = c("blue", "red"), legend = c("May", "Other months"))
 
 with(airquality, plot(Wind, Ozone, pch = 20), main = "Ozone and Wind in NYC")
-model <- lm(Wind ~ Ozone, airquality)
+model <- lm(Ozone ~ Wind, airquality)
 abline(model, lwd = 2)
+
+par(mfrow = c(1,2))
+with(airquality, {
+  plot(Wind, Ozone, main = "Ozone and Wind")
+  plot(Solar.R, Ozone, main = "Ozone and SOlar Radiation")
+})
+
+par(mfrow = c(1,3), mar = c(4,4,2,1), oma = c(0,0,2,0))
+with(airquality, {
+  plot(Wind, Ozone, main = "Ozone and Wind")
+  plot(Solar.R, Ozone, main = "Ozone and SOlar Radiation")
+  plot(Temp, Ozone, main = "Ozone and Temperature")
+  mtext("Ozone and Weather in NYC", outer = TRUE)
+})
+
