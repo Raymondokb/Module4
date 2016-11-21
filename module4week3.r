0fda00501134be567d37f2d91bd432ffa2de9899
Raymondokb <raymondokbmooc@gmail.com>
2016-11-21 16:58:32 +0800

    Add files via upload

--- module4week3.r
+++ module4week3.r
@@ -140,3 +140,213 @@ par(mfrow=c(1,3))
 image(t(constantMatrix)[,nrow(constantMatrix):1])
 plot(svd1$d,xlab="Column",ylab="Singular value",pch=19)
 plot(svd1$d^2/sum(svd1$d^2),xlab="Column",ylab="Prop. of variance explained",pch=19)
+
+
+svd1 <- svd(scale(dataMatrixOrdered))
+par(mfrow = c(1, 3))
+image(t(dataMatrixOrdered)[, nrow(dataMatrixOrdered):1])
+plot(svd1$u[, 1], 40:1, , xlab = "Row", ylab = "First left singular vector", pch = 19)
+plot(svd1$v[, 1], xlab = "Column", ylab = "First right singular vector", pch = 19)
+
+
+
+###############
+
+svd1 <- svd(scale(dataMatrixOrdered))
+#scale centers matrix
+#svd computes singular value decomposition of rectangular matrix
+#svd1 has d, u, v, and note that u%*%d%*%t(v)
+#dd <- svd1$u%*%diag(svd1$d)%*%t(svd1$v)
+par(mfrow = c(1,3))
+image(t(dataMatrixOrdered)[,nrow(dataMatrixOrdered):1])
+plot(svd1$u[,1], xlab = "Row", ylab = 'First left singular vector', pch = 19)
+plot(svd1$v[,1], xlab = "Column", ylab = "First right singular vector", pch = 19)
+
+
+#Variance explained
+par(mfrow = c(1,2))
+plot(svd1$d, xlab ="Column", ylab = 'Singu;lar value', pch = 19)
+plot(svd1$d^2/sum(svd1$d^2), xlab = "Column", ylab = "Prop. of variance explained", pch = 19)
+#note, first farleft point explains 40% of the data's variance.
+# High variance -> good
+
+
+#Relationship to Principal Components
+svd1 <- svd(scale(dataMatrixOrdered))
+pca1 <- prcomp(dataMatrixOrdered, scale = TRUE)
+plot(pca1$rotation[,1], svd1$v[,1],pch = 19, xlab = "Principal component 1", ylab = "Right singular Vector 1")
+abline(c(0,1))
+
+
+#part 2
+constantMatrix <- dataMatrixOrdered*0
+for(i in 1:dim(dataMatrixOrdered)[1]){constantMatrix[i,] <- rep(c(0,1), each = 5)}
+svd1 <- svd(constantMatrix)
+par(mfrow = c(1,3))
+image(t(constantMatrix)[, nrow(constantMatrix):1])
+plot(svd1$d, xlb = "Column", ylab = "Singular Value", pch = 19)
+plot(svd1$d^2/sum(svd1$d^2), xlab = "Column", ylab = "Proportion of variance explained", pch = 19)
+# since constantMatrix's columns are constant, svd1$d values for 2 onwards are very small,
+# implying that only the first v column has captures significant variance of data
+
+set.seed(678910)
+for(i in 1:40){
+ #flip a coin
+  coinFlip1 <- rbinom(1, size = 1, prob = 0.5)
+  coinFlip2 <- rbinom(1, size = 1, prob = 0.5)
+  
+  #if coin is heads add a common pattern t that row
+  if(coinFlip1){
+    dataMatrix[i, ] <- dataMatrix[i, ] + rep(c(0,5), each = 5)
+  }
+  if (coinFlip2){
+    dataMatrix[i, ] <- dataMatrix[i, ] + rep(c(0,5), 5)
+  }
+}
+#dist is distance matrix computation, compute distances between rows of a data matrix
+hh <- hclust(dist(dataMatrix))
+dataMatrixOrdered <- dataMatrix[hh$order,]
+
+
+#True patterns
+svd2 <- svd(scale(dataMatrixOrdered))
+par(mfrow = c(1,3))
+image(t(dataMatrixOrdered)[, nrow(dataMatrixOrdered):1])
+plot(rep(c(0,1), each = 5), pch = 19, xlab = "Column", ylab = "Pattern 1")
+plot(rep(c(0,1), 5), pch = 19, xlab = "Column", ylab = "Pattern 2")
+
+
+#v and patterns of variances in rows
+svd2 <- svd(scale(dataMatrixOrdered))
+par(mfrow=c(1,3))
+image(t(dataMatrixOrdered)[, nrow(dataMatrixOrdered):1])
+plot(svd2$v[, 1], pch = 19, xlab = "Column ", ylab = "First right singular vector")
+plot(svd2$v[, 2], pch = 19, xlab = "Column ", ylab = "First right singular vector")
+
+#d anv variance explained
+svd1 <- svd(scale(dataMatrixOrdered))
+par(mfrow = c(1, 2))
+plot(svd1$d, xlab = "Column", ylab = "Singular Value", pch = 19)
+plot(svd1$d^2/sum(svd1$d^2), xlab = "Column", ylab = "Percentage of variance explained", pch = 19)
+
+
+
+#Color
+install.packages("RColorBrewer")
+library("RColorBrewer")
+mypalette<-brewer.pal(4,"Blues")
+image(1:7,1,as.matrix(1:7),col=mypalette,xlab="Greens (sequential)",
+      ylab="",xaxt="n",yaxt="n",bty="n")
+
+
+
+
+#https://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+#Module4Week4
+rr <- readRDS("D:\\Users\\msboon\\Desktop\\For Data science coursera\\Plots\\exdata%2Fdata%2FNEI_data (1)\\summarySCC_PM25.rds")
+ee <- readRDS("D:\\Users\\msboon\\Desktop\\For Data science coursera\\Plots\\exdata%2Fdata%2FNEI_data (1)\\Source_Classification_Code.rds")
+ee <- data.frame(ee, stringsAsFactors = FALSE)
+#1
+str(rr)
+table(rr$year)
+table(rr$Pollutant)
+#want total emission for each of the four years
+#thiking: use a bar plot or histogram
+
+#barplot
+#manually create
+store1 <- matrix(data=0, nrow = 4, ncol = 2); colnames(store1) <- c("Year", "Total PM2.5  emission")
+
+years <- unique(rr$year)
+for(i in 1:length(years)){
+  store1[i,1] <- years[i]
+  store1[i,2] <- sum(rr[which(rr$year==years[i]),4])
+}
+rownames(store1) <- store1[,1]
+barplot(store1[,2], main = "Total PM2.5 emissions for four years", col = store1[,2], xlab = "Years", ylab = "Total PM2.5 emission")
+
+#2
+#fips=="24510"
+#use which() to single out just Maryland
+rrM <- rr[which(rr$fips=="24510"),]
+store2 <- matrix(data=0, nrow = 4, ncol = 2); colnames(store1) <- c("Year", "Total PM2.5  emission")
+
+years <- unique(rrM$year)
+for(i in 1:length(years)){
+  store2[i,1] <- years[i]
+  store2[i,2] <- sum(rr[which(rrM$year==years[i]),4])
+}
+rownames(store2) <- store2[,1]
+barplot(store2[,2], main = "Total PM2.5 emissions for four years in Maryland", col = store2[,2], xlab = "Years", ylab = "Total PM2.5 emission")
+
+
+#3
+#for Baltimore
+library(ggplot2)
+library(reshape2)
+unique(rr$type)
+type1 <- unique(rr$type)
+#rrM
+#######Below one line is the answer
+ggplot(rrM, aes(factor(year), fill=type)) + geom_bar()+ggtitle("Sources of emission over 4 years")
+##
+ggplot(rrM, aes(factor(type), fill=factor(year))) + geom_bar()+ggtitle("Sources of emission over 4 years")
+
+#ggplot(rrM, aes(x = factor(type))) + geom_bar(stat="count")  + ggtitle("hi")
+i=1
+rrM2 <- rrM[which(rrM$type==type1[i]),]
+ggplot(rrM2, aes(x = factor(year))) + geom_bar(stat = "count", fill =brewer.pal(5,"Blues")[2:5]) + ggtitle(type1[i] )
+
+i=2
+rrM2 <- rrM[which(rrM$type==type1[i]),]
+ggplot(rrM2, aes(x = factor(year))) + geom_bar(stat = "count", fill =brewer.pal(5,"Blues")[2:5]) + ggtitle(type1[i] )
+
+i=3
+rrM2 <- rrM[which(rrM$type==type1[i]),]
+ggplot(rrM2, aes(x = factor(year))) + geom_bar(stat = "count", fill =brewer.pal(5,"Blues")[2:5]) + ggtitle(type1[i] )
+
+i=4
+rrM2 <- rrM[which(rrM$type==type1[i]),]
+ggplot(rrM2, aes(x = factor(year))) + geom_bar(stat = "count", fill =brewer.pal(5,"Blues")[2:5]) + ggtitle(type1[i] )
+
+
+
+
+#4
+##check ee variable to see which are COAL
+#store its respective SCC and do a simple which() on rr
+coalR <- t(data.frame(lapply(ee[grepl("Coal",ee$EI.Sector),1], as.character), stringsAsFactors = FALSE))
+
+
+store3 <- rr[which(rr$SCC==as.numeric(coalR[1,1])),]
+for(i in 2:length(coalR)){
+  store3 <- rbind(store3, rr[which(rr$SCC==as.numeric(coalR[i,1])),])
+}
+mstore3 <- matrix(data=0, nrow = 4, ncol = 2)
+years3 <- unique(store3$year)
+for(i in 1:length(years3)){
+  mstore3[i,1] <- years3[i]
+  mstore3[i,2] <- sum(store3[which(store3$year==years3[i]),4])
+}
+rownames(mstore3) <- mstore3[,1]
+barplot(mstore3[,2], main = "Total PM2.5 emissions for four years (coal-related)", col = mstore3[,2], xlab = "Years", ylab = "Total PM2.5 emission (coal-related)")
+
+#5
+##get baltimore only
+rrM <- rr[which(rr$fips=="24510"),]
+vehicle <- t(data.frame(lapply(ee[grepl("Vehicle",ee$EI.Sector),1], as.character), stringsAsFactors = FALSE))
+
+store4 <- rr[which(rrM$SCC==as.numeric(vehicle[1,1])),]
+for(i in 2:length(vehicle)){
+  store4 <- rbind(store4, rrM[which(rrM$SCC==as.numeric(vehicle[i,1])),])
+}
+mstore4 <- matrix(data=0, nrow = 4, ncol = 2)
+years4 <- unique(store4$year)
+for(i in 1:length(years4)){
+  mstore4[i,1] <- years4[i]
+  mstore4[i,2] <- sum(store4[which(store4$year==years4[i]),4])
+}
+rownames(mstore4) <- mstore4[,1]
+barplot(mstore4[,2], main = "Total PM2.5 emissions for four years (vehicle-related)", col = mstore4[,2], xlab = "Years", ylab = "Total PM2.5 emission (vehicle-related)")
+
